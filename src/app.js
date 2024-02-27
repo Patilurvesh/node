c7085a7c7a3c6b9a130415ab63ba764796016b91
Patilurvesh <patilurvesh98@gmail.com>
2024-02-27 15:47:57 +0530

    first change

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.use('/', indexRouter);
 
 // Start the server
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
